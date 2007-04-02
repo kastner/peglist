68dfce2287320858bcaf19b2d3afd3ed1d2e9722
Erik Kastner <kastner@gmail.com>
2007-04-02 03:21:42 +0000

    good changes, css and now functionality

--- peglist.rb
+++ peglist.rb
@@ -1,10 +1,7 @@
 #!/usr/bin/env ruby
 
 $:.unshift File.dirname(__FILE__) + "/../../lib"
-require 'camping'
-require 'camping/session'
-require 'openid'
-require 'face'
+%w|camping camping/session openid face redcloth|.each{|lib| require lib}
 
 Camping.goes :Peglist
 
@@ -32,6 +29,7 @@ module Peglist::Models
   
   class User < Base
     validates_uniqueness_of :username
+    has_many :pegs
   end
   
   class CreateTheBasics < V 1.0
@@ -61,6 +59,8 @@ end
 module Peglist::Controllers
   class Index < R '/'
     def get
+      # raise @state.to_s
+      @user = User.find_by_id(@state.user_id, :include => :pegs) if @state.user_id
       render :index
     end
   end
@@ -71,13 +71,35 @@ module Peglist::Controllers
     end
   end
   
+  class New
+    def get
+      if @state.user_id
+        @peg = Peg.new
+        @peg.user_id = @state.user_id
+        render :new
+      else
+        render :user_error
+      end
+    end
+  end
+  
+  class Manynew
+    def get
+      if @state.user_id
+        render :many_new
+      else
+        render :user_error
+      end
+    end
+  end
+  
   class Signup
     def get
       if !@state.openid
         @error = "You must sign in with OpenID before you sign up"
         render :index
       else
-        @user = User.new
+        @new_user = User.new
         render :signup
       end
     end
@@ -86,10 +108,10 @@ module Peglist::Controllers
       @username = input.username
       @avatar_url = input.avatar_url
       
-      @user = User.create(:openid => @state.openid, :username => @username, :avatar_url => @avatar_url)
-      if @user.save
-        @state.user_id = @user.id
-        @state.username = @user.username
+      @new_user = User.create(:openid => @state.openid, :username => @username, :avatar_url => @avatar_url)
+      if @new_user.save
+        @state.user_id = @new_user.id
+        @state.username = @new_user.username
         redirect HURL(Index).to_s
       else
         render :signup
@@ -132,6 +154,7 @@ module Peglist::Controllers
         @state.openid = identity_url
         @user = User.find_by_openid(identity_url)
         if @user
+          @state.user_id = @user.id
           @state.username = @user.username
           redirect HURL(Index).to_s
         else
@@ -148,18 +171,14 @@ module Peglist::Controllers
       when OpenID::FAILURE
         @a = "Failure with that OpenID url. Check it and try again please."
       when OpenID::SUCCESS
-        # redirect response.redirect_url("http:" + self.URL.to_s, "http:" + self.URL(Login).to_s)
         redirect response.redirect_url(self.HURL.to_s, self.HURL(Login).to_s)
-        # redirect response.redirect_url("http://camping.metaatem.net/", "http://camping.metaatem.net/login/")
       end      
     end
   end
 
   class Logout
     def get
-      @state.openid = nil
-      @state.user_id = nil
-      @state.username = nil
+      @state.keys.each{|key| @state.delete(key)}
       @message = "You have been logged out"
       render :index
     end
@@ -181,6 +200,13 @@ module Peglist::Controllers
     end
   end
   
+  class Favicon < R '/favicon.ico'
+    def get
+      @headers['Content-Type'] = 'image/vnd.microsoft.icon'
+      @header['X-Sendfile'] = "#{PATH}/static/favicon.ico"
+    end
+  end
+  
   class AvatarSearch < R '/avatar/(.+)/(.+)'
     def get(service, username)
       case service
@@ -194,8 +220,12 @@ module Peglist::Controllers
 end
 
 module Peglist::Views
-  def bob
-    p @a
+  def amp
+    span.amp "&"
+  end
+  
+  def textalize(str)
+    text RedCloth.new(str).to_html
   end
   
   def layout
@@ -208,13 +238,19 @@ module Peglist::Views
       body.home! do
         div.page! do
           div.header! do
-            h2.logo! { a 'Peg list at Meta | ateM', :href => R(Index)}
-            if !@user
-              _login
+            h2.logo! do
+              text %Q{<a href="/" title='Peg list at Meta | ateM'><img src="/static/logo.png" alt="Peg list at Meta | ateM"/></a>}
             end
-            ul.nav! do
-              li.home { a("Home / Peg list", :href => R(Index)) }
-              li.what { a "What is a Peg list?", :href => R(What) }
+            div.wrap do
+              hr
+              if !@new_user
+                _login
+              end
+              hr
+              ul.nav! do
+                li.home { text %Q{<a href="/">Home #{amp} Peg list</a>} }
+                li.what { a "What is a Peg list?", :href => R(What) }
+              end
             end
           end
           div.content! do
@@ -229,22 +265,97 @@ module Peglist::Views
   end
   
   def index
-    p @a
+    if @state.username
+      _logged_in_home
+    else
+      _new_home
+    end
+  end
+  
+  def new
+    h1 "Add a peg"
+  end
+  
+  def many_new
+    h1 "Add multiple pegs"
+    
+  end
+  
+  def user_error
+    h1 "You must be logged in to do that. Log in above by using an OpenID address."
+  end
+  
+  def _logged_in_home
+    h1 do
+      img :src => @user.avatar_url if @user.avatar_url?
+      text "#{@user.username}'s Peg list"
+    end
+    a "Add a peg", :href => R(New)
+    text " or "
+    a "Add many pegs", :href => R(Manynew)
+  end
+  
+  def _new_home
+    textalize <<-HTML
+    h1. Welcome to Peg list from Meta | ateM
+    
+    A peg list is a memory device that has been used for thousands of years.
+    
+    For more information on peg lists and why they are useful for anyone, <a href="/what">read "What is a peg list"</a>.
+    
+    Pet list from Meta | ateM will let you record your peg list, and associate each peg with an image to help strengthen the association -- peg lists work best when your form pictures in your mind.
+    
+    If you're ready to get started -- log in at the top of the page with any "OpenID":http://http://en.wikipedia.org/wiki/OpenID address.
+    HTML
   end
   
   def what
+    textalize <<-HTML
+    h1. What is a Peg list?
+    
+    A peg system is a mnemonic technique for memorizing lists. It works by pre-memorizing a list of words that are easy to associate with the numbers they represent(1 to 10, 1-100, 1-1000, etc). Those objects form the "pegs" of the system. Then in the future, to rapidly memorize a list of arbitrary objects, each one is associated with the appropriate peg. Generally, a peglist only has to be memorized one time, and can then be used over and over every time a list of items needs to be memorized.
+    
+    taken from "http://en.wikipedia.org/wiki/Mnemonic_peg_system":Wikipedia
+    
+    The short version: you spend time in the beginning learning a list of words that you later use as anchors for memorizing OTHER things. By working on this memory skill, not only are you able to memorize lists in day-to-day life (like grocerys), it helps you with your normal memory as well.
+    
+    Here's an example. Let's pretend your peg list has these items as it's first five:
+    
+    # gun
+    # shoe
+    # tree
+    # door
+    # hive
+    
+    and the list you need to memorize is:
+    
+    # tomatos
+    # milk
+    # bread
+    # toothpaste
+    # toilet paper
+    
+    You'd go through the list and form the silliest pictures you can relating one to the other.
+    For example: 
+    
+    !http://farm1.static.flickr.com/36/78546217_ab4d8a0387_m.jpg(Bullet against tomato on Flickr)!:http://www.flickr.com/photos/fotofrog/78546217/
+    
+    and so on (stepping on a jug of milk, a tree with loaves of bread hanging from it, etc.)
+    
+    
+    HTML
   end
   
   def signup
     h1 "Pick your username and an avatar if you have one"
     h2 "You will use the OpenID #{@state.openid} to log in"
     
-    errors_for @user
+    errors_for @new_user
     
     form :action => R(Signup), :method => :post do
       p do
         label 'Username:', :for => 'signup_username'
-        input :name => 'username', :type => 'text', :id => 'signup_username', :value => @user.username
+        input :name => 'username', :type => 'text', :id => 'signup_username', :value => @new_user.username
       end
       
       fieldset do
@@ -299,13 +410,14 @@ module Peglist::Views
     if !@state.username or @state.username.empty?
       form :action => R(Login), :method => :post do
         div do
-          label 'your OpenID address', :for => 'login_openid'
+          label 'your OpenID address', :for => 'login_openid', :id => 'login_openid_label'
           input :name => 'openid_url', :type => 'text', :id => 'login_openid'
         
           input :type => 'submit', :value => 'login'
           text <<-HTML
           <script type="text/javascript" charset="utf-8">
             window.addEvent('domready', function() { if ($('login_openid').value != '') {$('login_openid').setStyle('zIndex', 3) }})
+            $('login_openid_label').addEvent('click', function() { $('login_openid').focus(); })
             $('login_openid').addEvent('focus', function() { $('login_openid').setStyle('zIndex', 3) })
             $('login_openid').addEvent('blur', function() { if ($('login_openid').value == '') $('login_openid').setStyle('zIndex', 1) })
           </script>
